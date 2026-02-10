bba9c86b11def2f2102210dfe681effd03f91863
Diana2716 <dianaoliveirandrade@gmail.com>
2026-02-09 22:02:41 -0300

    feat: learning js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const nome = "Nikollas";
+const name = "Nikollas";
 const idade = "18";
 const soma = 7 + 3;
 
-console.log("Nome:", nome);
+console.log("Nome:", name);
 console.log("Idade:", idade);
 console.log("Soma de 7 + 3:", soma);
